7916d41fb95044dc69c3d4ee17f3d0c0dcb55ea2
Baptiste Lemoine <b.lemoine14@laposte.net>
2020-03-08 23:28:24 +0100

    delete starter content and unused cpp file

--- Source/Tindalos/TindalosGameMode.cpp
+++ Source/Tindalos/TindalosGameMode.cpp
@@ -1,7 +1,6 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "TindalosGameMode.h"
-#include "TindalosPawn.h"
 #include "UObject/ConstructorHelpers.h"
 
 ATindalosGameMode::ATindalosGameMode()
